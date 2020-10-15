6e1c36365bd09d8e65410176b16075dccf67afdc
Omar Amin <omar_75@hotmail.dk>
2020-10-15 13:14:55 +0200

    Initializing project

---
+++ pages/api/db.js
@@ -0,0 +1,19 @@
+const postgres = require('postgres')
+
+const sql = postgres({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'db',
+    password: '',
+    port: 5432
+})
+
+export default async (req, res) => {
+    console.log(req.body)
+    res.statusCode = 200
+    const users = await sql`
+        SELECT * FROM users
+    `
+    res.json(users)
+
+}
