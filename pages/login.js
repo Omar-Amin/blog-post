650837be0d3512c91e58cab08de12fb124b081c5
Omar Amin <omar_75@hotmail.dk>
2020-10-15 20:18:50 +0200

    Added hyperlink from homepage to login

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function LogIn() {
+
+
+    return (
+        <div className={styles.container}>
+            log in
+        </div>
+    )
+}
